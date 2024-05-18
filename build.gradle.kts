eb41d94fb8812585cf26cb96f0c748a22c32e25c
Márton Braun <marton.braun@jetbrains.com>
2024-05-18 18:15:57 +0200

    Update Gradle version, inline dependencies from buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version kotlinVersion
+    kotlin("jvm") version "1.7.10"
 }
 
 tasks.withType<KotlinCompile>() {
@@ -29,27 +29,26 @@ repositories {
 }
 
 dependencies {
-    implementation(Deps.ktorServerCore)
-    implementation(Deps.ktorServerNetty)
-    implementation(Deps.ktorAuth)
-    implementation(Deps.ktorAuthJwt)
-    implementation(Deps.ktorContentNegotiation)
-    implementation(Deps.ktorJackson)
-    implementation(Deps.ktorDefaultHeaders)
-    implementation(Deps.ktorCors)
-    implementation(Deps.ktorCallLogging)
-    implementation(Deps.ktorStatusPages)
+    implementation("io.ktor:ktor-server-core:2.0.3")
+    implementation("io.ktor:ktor-server-netty:2.0.3")
+    implementation("io.ktor:ktor-server-auth:2.0.3")
+    implementation("io.ktor:ktor-server-auth-jwt:2.0.3")
+    implementation("io.ktor:ktor-server-content-negotiation:2.0.3")
+    implementation("io.ktor:ktor-serialization-jackson:2.0.3")
+    implementation("io.ktor:ktor-server-default-headers:2.0.3")
+    implementation("io.ktor:ktor-server-cors:2.0.3")
+    implementation("io.ktor:ktor-server-call-logging:2.0.3")
+    implementation("io.ktor:ktor-server-status-pages:2.0.3")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("com.h2database:h2:2.1.214")
+    implementation("org.jetbrains.exposed:exposed-core:0.38.2")
+    implementation("org.jetbrains.exposed:exposed-dao:0.38.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.38.2")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("io.insert-koin:koin-ktor:3.2.0")
 
-    implementation(Deps.logback)
-    implementation(Deps.h2Database)
-    implementation(Deps.exposedCore)
-    implementation(Deps.exposedDao)
-    implementation(Deps.exposedJdbc)
-    implementation(Deps.exposedJavaTime)
-    implementation(Deps.hikari)
-    implementation(Deps.koin)
-
-    testImplementation(Deps.kotlinTests)
-    testImplementation(Deps.ktorTests)
-    testImplementation(Deps.ktorClientContentNegotiation)
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.10")
+    testImplementation("io.ktor:ktor-server-test-host:2.0.3")
+    testImplementation("io.ktor:ktor-client-content-negotiation:2.0.3")
 }
